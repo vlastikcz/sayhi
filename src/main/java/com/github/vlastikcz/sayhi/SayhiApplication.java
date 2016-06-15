b25a6091c520696d130c8952308a22aae1dc90ff
Kotas Vlastimil <Vlastimil.Kotas@ysoft.com>
2016-06-15 19:39:53 +0200

    Initial commit - generated project with basic settings

---
+++ src/main/java/com/github/vlastikcz/sayhi/SayhiApplication.java
@@ -0,0 +1,12 @@
+package com.github.vlastikcz.sayhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayHiApplication.class, args);
+	}
+}
